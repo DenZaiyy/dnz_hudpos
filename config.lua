bfb36bf3b2b657090b1de5e0ffe1f291b6ec3a79
DenZaiyy <denzaiyy@outlook.fr>
2024-06-01 01:53:31 +0200

    add global config

--- config.lua
+++ config.lua
@@ -2,3 +2,10 @@ Config = {}
 Config.Locale = GetConvar("esx:locale", "en")
 
 Config.Enable_Command = true
+
+-- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.Keybind_Focus = 'MULTIPLY'
+Config.Keybind_ToggleHUD = 'PAGEDOWN'
+
+
+Config.HUD_Position = 'top-right' -- top-left, top-right, bottom-left, bottom-right, top-middle
